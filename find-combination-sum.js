631101b9e8afa60eb773ee259e9dee864a904fed
sossw1 <sossw1@gmail.com>
2021-08-21 11:21:24 -0400

    Solution ideas

--- find-combination-sum.js
+++ find-combination-sum.js
@@ -1,6 +1,14 @@
 // Write a function that returns a boolean indicating whether there are two
-// distinct numbers in an array whose sum is a given length.
+// distinct numbers in an array whose sum is a target value.
 
-function findCombination(totalLength, arrayOfLengths) {
+function findCombination(target, array) {
+  // Idea 1: Nested loop to compare all combos
+  // Time: O(n^2) Space O(1)
+
+  // Idea 2: Sum implies difference from target. Using a set, we can tell if a
+  // previous element is the difference between the target and the current value.
+  // If not, add the number to the set if it is not already part of it.
+  // Time: O(n) Space: O(n)
+
   
 }
