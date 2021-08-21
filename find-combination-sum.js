3c68af09eb22d3ceecdfd841d21d0c3d2ff341ad
sossw1 <sossw1@gmail.com>
2021-08-21 07:24:42 -0400

    Problem outline

---
+++ find-combination-sum.js
@@ -0,0 +1,6 @@
+// Write a function that returns a boolean indicating whether there are two
+// distinct numbers in an array whose sum is a given length.
+
+function findCombination(totalLength, arrayOfLengths) {
+  
+}
