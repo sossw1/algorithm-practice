cc855e60da08895265420111ffe840950be0c3df
sossw1 <sossw1@gmail.com>
2021-08-23 19:45:35 -0400

    Base problem

---
+++ check-permutation.js
@@ -0,0 +1,6 @@
+// Write an efficient function that checks whether any permutation of an input
+// string is a palindrome. Assume the input string only include lowercase letters.
+
+function checkPalindrome(str) {
+  
+}
