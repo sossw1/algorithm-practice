31e135b806d2cb02e5049c3b0f6f429d8df23f7c
sossw1 <sossw1@gmail.com>
2021-08-23 19:52:08 -0400

    Idea for algorithm using a set

--- check-permutation.js
+++ check-permutation.js
@@ -2,5 +2,13 @@
 // string is a palindrome. Assume the input string only include lowercase letters.
 
 function checkPalindrome(str) {
+  // Idea: palindromes have at most 1 character which occurs an odd number
+  // of times. We can use a set to collect occurrences of letters. Loop through
+  // the input string, check if the set has the letter. If so, remove the letter
+  // from the set and if not, add it to the set. At the end, check if the set
+  // contains multiple letters. If it contains 0 or 1 element, this can be
+  // permutated into a palindrome.
+  // Efficiency: Time O(n), Space O(n)
+
   
 }
