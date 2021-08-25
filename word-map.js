46c177afd7c04c00e0d842667a39c1d0be161090
sossw1 <sossw1@gmail.com>
2021-08-25 12:17:07 -0400

    Base problem

---
+++ word-map.js
@@ -0,0 +1,8 @@
+/*
+Write a function that takes a long string and returns a map, where the keys are
+words and the values are the number of times the words occurred.
+*/
+
+function wordMap() {
+  
+}
