353db040d2c7ef3e4b6c7eb2b18bdfb2f627dd95
sossw1 <sossw1@gmail.com>
2021-08-25 15:01:12 -0400

    First idea

--- word-map.js
+++ word-map.js
@@ -3,6 +3,15 @@ Write a function that takes a long string and returns a map, where the keys are
 words and the values are the number of times the words occurred.
 */
 
-function wordMap() {
+function wordMap(inputString) {
+  /*
+  For each word of the input string, accumulate on a key inside the map while
+  creating new keys for words that have not yet occurred in the string. Make
+  sure to skip over spaces, periods, etc.
+
+  Idea: Use array's built in split method to retrieve array of words and
+  punctuation, then toLowerCase, then run through array with typeOf string.
+  */
   
+
 }
