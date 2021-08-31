3fc1836fac896ec2c6ecc02d49b1969b8a0318d6
sossw1 <sossw1@gmail.com>
2021-08-31 14:08:16 -0400

    Base problem

---
+++ best-stock-profit.js
@@ -0,0 +1,7 @@
+/* Given an array of stock prices where indexes correspond to minutes since
+opening time, write a function that returns the best possible profit for one
+purchase and one sale of stock without short-selling the stock. */
+
+function bestProfit(priceArray) {
+
+}
