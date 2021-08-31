983acc850220ea6477c3e41aa586132dea1e0786
sossw1 <sossw1@gmail.com>
2021-08-31 16:20:19 -0400

    Ideas

--- highest-product-triple.js
+++ highest-product-triple.js
@@ -2,5 +2,8 @@
 with three of them. */
 
 function findHighestProductTriple(arrayOfInts) {
-  
+  // Idea: Sorting and selecting three highest integers
+
+  // Idea 2: Greedy walk through array, storing in another array and updating
+  // when a value exceeds one of the current three highest values
 }
