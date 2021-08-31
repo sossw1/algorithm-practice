f3e3ba635569eb527b76ef102cf4bbbb8ae59848
sossw1 <sossw1@gmail.com>
2021-08-31 14:20:30 -0400

    Idea using greedy algorithm

--- best-stock-profit.js
+++ best-stock-profit.js
@@ -3,5 +3,8 @@ opening time, write a function that returns the best possible profit for one
 purchase and one sale of stock without short-selling the stock. */
 
 function bestProfit(priceArray) {
+/* Idea: Using greedy algorithm, walk through array and compare profit at each
+index to previous possible profit values and keep the better option. */
 
+  
 }
