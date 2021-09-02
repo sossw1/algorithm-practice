961c47dc07af51c79e6642b6192eb3016b6fb532
sossw1 <sossw1@gmail.com>
2021-09-02 11:20:55 -0400

    Wrote function

--- highest-product-triple.js
+++ highest-product-triple.js
@@ -17,8 +17,57 @@ with three of them.
   be the result of the highest number times either the product of the two next
   highest numbers or the two lowest numbers. Track these intermediate values
   along the way.
+
+  Time: O(n), Space: O(1)
 */
 
 function findHighestProductTriple(arrayOfInts) {
-  
+  // Test if input at least 3 integers
+  if (arrayOfInts.length < 3) {
+    throw new Error('Must input at least 3 integers');
+  }
+
+  let highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+  let lowest = Math.min(arrayOfInts[0], arrayOfInts[1]);
+
+  let highestProductTwo = arrayOfInts[0] * arrayOfInts[1];
+  let lowestProductTwo = arrayOfInts[0] * arrayOfInts[1];
+
+  let highestProductThree = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+  for (let i = 2; i < arrayOfInts.length; i++) {
+    // check current value's two products against current highest result
+    // update highest and lowest products
+    // update highest and lowest numbers
+
+    let current = arrayOfInts[i];
+    // Check if we have a new highest triple using the current number
+    highestProductThree = Math.max(
+      highestProductThree,
+      current * highestProductTwo,
+      current * lowestProductTwo
+    );
+
+    // Check if we have a new highest product of two
+    highestProductTwo = Math.max(
+      highestProductTwo,
+      current * highest,
+      current * lowest
+    );
+
+    // Check if we have a new lowest product of two
+    lowestProductTwo = Math.min(
+      lowestProductTwo,
+      current * highest,
+      current * lowest
+    );
+
+    // Check if we have a new highest value
+    highest = Math.max(highest, current);
+
+    // Check if we have a new lowest value
+    lowest = Math.min(lowest, current);
+
+  }
+  return highestProductThree;
 }
