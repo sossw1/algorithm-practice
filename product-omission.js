daa224b561cc7470bac4febc1633d04f9168badf
sossw1 <sossw1@gmail.com>
2021-09-02 11:41:00 -0400

    Base problem

---
+++ product-omission.js
@@ -0,0 +1,12 @@
+/*
+Given an array of integers, return an array where the value at each index is the
+product of all values in the given array except the value at the corresponding
+index. Do not use division in the algorithm.
+Ex: f([1, 2, 3]) = [6, 3, 2]
+
+
+*/
+
+function getProductOfAllIntsExceptAtIndex(arrayOfInts) {
+
+}
