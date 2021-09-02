632b243e7dad80287718cc4391e34e03558b0cbe
sossw1 <sossw1@gmail.com>
2021-09-02 12:27:58 -0400

    Idea 1 greedy algorithm

--- product-omission.js
+++ product-omission.js
@@ -4,7 +4,9 @@ product of all values in the given array except the value at the corresponding
 index. Do not use division in the algorithm.
 Ex: f([1, 2, 3]) = [6, 3, 2]
 
-
+  Idea 1: Walk forward through array. At each step, accumulate product of prior
+  indexes. Then walk backward through array, accumulating product of later
+  indexes. Multiply the corresponding values and store in a new array.
 */
 
 function getProductOfAllIntsExceptAtIndex(arrayOfInts) {
