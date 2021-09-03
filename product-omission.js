985ae3e3bc5e64415490ca5439d010794bac2af5
sossw1 <sossw1@gmail.com>
2021-09-03 15:15:54 -0400

    Time and space efficiency

--- product-omission.js
+++ product-omission.js
@@ -7,6 +7,8 @@ Ex: f([1, 2, 3]) = [6, 3, 2]
   Idea 1: Walk forward through array. At each step, accumulate product of prior
   indexes. Then walk backward through array, accumulating product of later
   indexes. Multiply the corresponding values and store in a new array.
+
+  Time: O(n), Space O(n)
 */
 
 function getProductOfAllIntsExceptAtIndex(arrayOfInts) {
