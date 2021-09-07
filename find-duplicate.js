aedd77c53afa8bb0585a0bc4b10c18e4b82593f1
sossw1 <sossw1@gmail.com>
2021-09-07 13:57:34 -0400

    Idea 1

--- find-duplicate.js
+++ find-duplicate.js
@@ -2,6 +2,13 @@
 Given an array of integers between 1 and n, of size n+1, write a function which
 finds an integer that appears more than once in the array without modifying the
 input array. Optimize the solution for space efficiency.
+
+  Idea 1: To use minimal space, sort the array first, then compare neighboring
+  elements until a duplicate is found. O(1) space, O(n log n) time.
+    Unfortunately, this would either require an additional array of size n in
+    memory, or we would be modifying the input array.
+  
+  
 */
 
 function findDuplicate(arrayOfInts) {
