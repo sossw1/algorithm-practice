59237cde050c698c32a837435390beb46508663d
sossw1 <sossw1@gmail.com>
2021-09-07 11:28:22 -0400

    Idea 2

--- find-rotation-point.js
+++ find-rotation-point.js
@@ -5,6 +5,9 @@ a function that finds this rotation point.
 
   Idea 1: Since the array is sorted aside from the rotation, we can loop through
   the array and check if each word is greater than the previous word.
+
+  Idea 2: At each step, compare first word of current selection to the middle
+  word to see if the rotation point is to the left or right.
 */
 
 function findRotationPoint(arrayOfWords) {
