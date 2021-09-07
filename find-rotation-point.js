9592d920eb04b4089eb661a2fbd3cbdbe71a1654
sossw1 <sossw1@gmail.com>
2021-09-07 11:04:53 -0400

    Base problem

---
+++ find-rotation-point.js
@@ -0,0 +1,9 @@
+/*
+You are given an array of words that has been sorted alphabetically and then
+'rotated' at a random index where the two pieces of the array are swapped. Write
+a function that finds this rotation point.
+*/
+
+function findRotationPoint(arrayOfWords) {
+  
+}
