08040885790fa2d2bc03e9de52685e1100580c43
sossw1 <sossw1@gmail.com>
2021-09-07 11:07:50 -0400

    Idea 1

--- find-rotation-point.js
+++ find-rotation-point.js
@@ -2,6 +2,9 @@
 You are given an array of words that has been sorted alphabetically and then
 'rotated' at a random index where the two pieces of the array are swapped. Write
 a function that finds this rotation point.
+
+  Idea 1: Since the array is sorted aside from the rotation, we can loop through
+  the array and check if each word is greater than the previous word.
 */
 
 function findRotationPoint(arrayOfWords) {
