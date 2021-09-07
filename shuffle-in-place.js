d194178633bf732e48a6e4286a64cdef4ab43148
sossw1 <sossw1@gmail.com>
2021-09-07 09:37:24 -0400

    swap helper function

--- shuffle-in-place.js
+++ shuffle-in-place.js
@@ -2,9 +2,15 @@
 Write a function that performs an in-place shuffle of an array.
 */
 
-function shuffle() {
+function shuffle(array) {
   function getRandom(floor, ceiling) {
     return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
   }
 
+  function swap(array, firstIndex, secondIndex) {
+    let placeholder = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = placeholder;
+  }
+  
 }
