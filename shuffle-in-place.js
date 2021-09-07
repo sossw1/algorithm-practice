a20f2ebbe652c99460d1c405bf85fb79629727f8
sossw1 <sossw1@gmail.com>
2021-09-07 09:27:09 -0400

    Helper function for random numbers

--- shuffle-in-place.js
+++ shuffle-in-place.js
@@ -3,5 +3,8 @@ Write a function that performs an in-place shuffle of an array.
 */
 
 function shuffle() {
+  function getRandom(floor, ceiling) {
+    return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
+  }
 
 }
