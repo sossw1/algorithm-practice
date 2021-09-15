cd557809a4b8a10d0bc5dbef78ba8fa4a6fee125
sossw1 <sossw1@gmail.com>
2021-09-15 11:12:40 -0400

    Idea 1

--- sort-scores.js
+++ sort-scores.js
@@ -1,6 +1,7 @@
 /*
-Write a function that takes an array of scores and the highest possible score
-and returns a sorted array of scores in less than O(n log n) time.
+Write a function that takes an array of scores and the highest possible score and returns a sorted array of scores in less than O(n log n) time.
+
+  Idea 1: Using a hash map (object) we can store how many times each value occurs, then walk through possible values again in order to write the ordered array. This takes up O(n) space but gets time down to O(n).
 */
 
 function sortScores(arrayOfScores, highestPossibleScore) {
