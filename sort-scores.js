91cb2fdc34d0d5296010272297d62fb7fad730df
sossw1 <sossw1@gmail.com>
2021-09-15 12:58:09 -0400

    use array instead of object for counting

--- sort-scores.js
+++ sort-scores.js
@@ -2,24 +2,20 @@
 Write a function that takes an array of scores and the highest possible score and returns a sorted array of scores in less than O(n log n) time.
 
   Idea 1: Using a hash map (object) we can store how many times each value occurs, then walk through possible values again in order to write the ordered array. This takes up O(n) space but gets time down to O(n).
+
+  Idea 2: Use an array for counting instead to improve time.
 */
 
 function sortScores(arrayOfScores, highestPossibleScore) {
-  const scores = {};
+  const scoreCounts = new Array(highestPossibleScore + 1).fill(0);
   const sortedScores = [];
   arrayOfScores.forEach((item) => {
-    if(scores.hasOwnProperty([item])) {
-      scores[item] += 1;
-    } else {
-      scores[item] = 1;
-    }
+    scoreCounts[item] += 1;
   });
   
   for(let i = highestPossibleScore; i >= 0; i--) {
-    if(scores.hasOwnProperty([i])) {
-      for(let j = 0; j < scores[i]; j++) {
-        sortedScores.push(i);
-      }
+    for(let j = 0; j < scoreCounts[i]; j++) {
+      sortedScores.push(i);
     }
   }
   return sortedScores;
