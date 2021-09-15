6b553f2c3e5ca53adcd05e25663bb217d86d158a
sossw1 <sossw1@gmail.com>
2021-09-15 10:45:33 -0400

    base problem

---
+++ sort-scores.js
@@ -0,0 +1,8 @@
+/*
+Write a function that takes an array of scores and the highest possible score
+and returns a sorted array of scores in less than O(n log n) time.
+*/
+
+function sortScores(arrayOfScores, highestPossibleScore) {
+  
+}
