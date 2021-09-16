e9ec87298b5e2fd9df8def5f8cb24fb94ffe413b
sossw1 <sossw1@gmail.com>
2021-09-16 11:13:56 -0400

    base problem

---
+++ super-balance.js
@@ -0,0 +1,7 @@
+/*
+Write a function to check if a binary tree is "superbalanced", meaning the difference between the depth of any two leaf nodes is no greater than one.
+*/
+
+function isSuperBalanced(binaryTree) {
+
+}
