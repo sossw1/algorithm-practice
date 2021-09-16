25cd3cfa57ed20d2c3868c89ec1840877bf67b6d
sossw1 <sossw1@gmail.com>
2021-09-16 14:05:44 -0400

    Idea 2

--- super-balance.js
+++ super-balance.js
@@ -1,7 +1,9 @@
 /*
 Write a function to check if a binary tree is "superbalanced", meaning the difference between the depth of any two leaf nodes is no greater than one.
 
-  Idea 1: Using breadth-first search, find the least-depth leaf node, then determine whether any branch has nodes beyond the next-lowest level.
+  Idea 1: Using breadth-first search (BFS), find the least-depth leaf node, then determine whether any branch has nodes beyond the next-lowest level.
+
+  Idea 2: Using depth-first search (DFS), we compare leaf depths and store the minimum leaf depth for comparison. DFS is more likely to fail faster than BFS, which helps improve the best-case scenario.
 */
 
 function isSuperBalanced(binaryTree) {
