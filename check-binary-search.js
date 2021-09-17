0e221d781ed930a98419eabe1180ad77fd3a1e36
sossw1 <sossw1@gmail.com>
2021-09-17 10:49:46 -0400

    BinaryTreeNode class

--- check-binary-search.js
+++ check-binary-search.js
@@ -2,6 +2,24 @@
 Problem: Write a function to check if a binary tree is a valid binary search tree.
 */
 
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
 function isBinarySearchTree(rootNode) {
   
 }
