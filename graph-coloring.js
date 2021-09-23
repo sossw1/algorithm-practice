dc8e2bc1311980320cca129651e1199393247408
sossw1 <sossw1@gmail.com>
2021-09-22 22:40:38 -0400

    GraphNode class

--- graph-coloring.js
+++ graph-coloring.js
@@ -2,6 +2,14 @@
 Write a function to find a graph coloring for an undirected graph with maximum degree D using at most D+1 colors.
 */
 
+class GraphNode {
+  constructor(label) {
+    this.label = label;
+    this.neighbors = new Set();
+    this.color = null;
+  }
+}
+
 function findColoring(graphNode) {
 
 }
