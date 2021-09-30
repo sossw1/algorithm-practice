68800e9c2716bdd4dff016addd49d0e65058f130
sossw1 <sossw1@gmail.com>
2021-09-30 16:13:26 -0400

    base problem

---
+++ trees-graphs/find-duplicate-graph.js
@@ -0,0 +1,13 @@
+/*
+**Note** This exercise demonstrates a more optimal solution of the same problem presented in a previously completed exercise in '/search-sort/find-duplicate.js'
+
+Given an array of integers between 1 and n, of size n+1, write a function which
+finds an integer that appears more than once in the array without modifying the
+input array. Optimize the solution for space efficiency.
+
+To start, imagine each item in the array as a node in a linked list, where each node has a value and an associated 'next' pointer.
+*/
+
+function findDuplicate(intArray) {
+
+}
