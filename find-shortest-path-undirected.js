fc1ee7008e233d6b79339588b26b0d0238011bd2
sossw1 <sossw1@gmail.com>
2021-09-29 22:03:21 -0400

    exception 1

--- find-shortest-path-undirected.js
+++ find-shortest-path-undirected.js
@@ -4,6 +4,9 @@ Given an undirected graph of friends in a network find a shortest route for a me
   Idea 1: Breadth-first search for the recipient, storing paths where the endpoint node has not been previously encountered in order to avoid cycles.
 
   Idea 2: Breadth-first search for the recipient, but instead of storing paths we store for each node which node came before it in the graph traversal. By using an object to store this data, we can use the keys to also track which nodes have been previously encountered in order to avoid cycles.
+
+  Exceptions:
+    1) If there is no path from the sender to the recipient, return null
 */
 
 function findShortestPath(graph, start, end) {
