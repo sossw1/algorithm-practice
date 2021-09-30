d69e6e37472799b8c5106588d1c42e121ff5fc48
sossw1 <sossw1@gmail.com>
2021-09-29 20:39:07 -0400

    base problem

---
+++ find-shortest-path-undirected.js
@@ -0,0 +1,7 @@
+/*
+Given an undirected graph of friends in a network find a shortest route for a message from a sender to a recipient. Return an array of friends that make up this path.
+*/
+
+function findShortestPath(graph, start, end) {
+
+}
