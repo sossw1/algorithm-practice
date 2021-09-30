36d14345eddd84150e62f97b81ce8393c4badcca
sossw1 <sossw1@gmail.com>
2021-09-29 21:54:00 -0400

    idea 1 rewrite

--- find-shortest-path-undirected.js
+++ find-shortest-path-undirected.js
@@ -1,7 +1,7 @@
 /*
 Given an undirected graph of friends in a network find a shortest route for a message from a sender to a recipient. Return an array of friends that make up this path.
 
-  Idea 1: For each path (initially just the sender's node), for each neighbor of its current endpoint, check if the neighbor is the recipient. If not, check if the neighbor has been encountered before. If not, store a new path with this neighbor as the endpoint.
+  Idea 1: Breadth-first search for the recipient, storing paths where the endpoint node has not been previously encountered in order to avoid cycles.
 */
 
 function findShortestPath(graph, start, end) {
