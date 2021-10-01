c6d95b744a749601b7d80df0f86c0ba0c63cd157
sossw1 <sossw1@gmail.com>
2021-10-01 12:55:30 -0400

    idea 2

--- trees-graphs/find-duplicate-graph.js
+++ trees-graphs/find-duplicate-graph.js
@@ -8,6 +8,15 @@ input array. Optimize the solution for space efficiency.
 To start, imagine each item in the array as a node in a linked list, where each node has a value and an associated 'next' pointer.
 
   Idea 1: Search for a node that has 2 inbound edges. That node's index is the value of a duplicate.
+
+  Idea 2: Traverse the array like a linked list, where each value in the input array points to the next index in the linked list.
+    There are n+1 nodes with values between 1 and n, so none of the values can be n+1. In terms of positions, none of the values can point to the n+1th position (i.e. the nth index of the array). Make n the head (starting) index, then traverse like a linked list using the values of each node.
+
+    In this traversal, cycles must necessarily exist. This can be proven by contradiction, similar to proving there must be a duplicate in this set of numbers.
+
+    In order to find a node with two inbound pointers (like in idea 1), the first node we encounter that is part of a cycle must have two pointers. We know this is true because we start from outside of the cycle and then enter the cycle.
+
+    To find this special node, first find the length of the cycle (l). We can do this by starting at the head and advancing n times, thus necessitating that we are inside a cycle. Using this position, advance until you get back to the same spot and find out how many steps it took, which is the cycle length. Then use two pointers, one at the head and one that is l spaces ahead, and advance them until they are at the same node.
 */
 
 function findDuplicate(intArray) {
