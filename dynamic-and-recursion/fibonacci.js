5928ba0b754f1af2873f094e2020b6916ea5d17e
sossw1 <sossw1@gmail.com>
2021-10-03 15:56:49 -0400

    iterative saves space on call stack over recursion

--- dynamic-and-recursion/fibonacci.js
+++ dynamic-and-recursion/fibonacci.js
@@ -8,5 +8,13 @@ function fib(n) {
     return n;
   }
 
-  return result = fib(n-1) + fib(n-2);
+  let result = 1;
+  let previousResult = 1;
+  let twoResultsAgo = 0;
+  for(let i = 2; i <= n; i++) {
+    result = previousResult + twoResultsAgo;
+    twoResultsAgo = previousResult;
+    previousResult = result;
+  }
+  return result;
 }
