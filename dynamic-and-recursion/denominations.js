9758ba296aa1d3dc4d7893b5b1d73fa8d24608b9
sossw1 <sossw1@gmail.com>
2021-10-04 13:15:34 -0400

    idea 2

--- dynamic-and-recursion/denominations.js
+++ dynamic-and-recursion/denominations.js
@@ -2,6 +2,8 @@
 Given a total and an array of integers representing coin denominations, write a function to find the number of ways to make the total with the coins of the available denominations.
 
   Idea 1: Recursively compose set of solutions iterating from 1 to total.
+
+  Idea 2: Build an array for number of solutions from bottom up.
 */
 
 function numCurrencyCombinations(denominations, total) {
