d77f673fd4fb55adc26e62f571ed9feda69b1ad1
sossw1 <sossw1@gmail.com>
2021-10-04 14:57:21 -0400

    base problem

---
+++ dynamic-and-recursion/weighted-value.js
@@ -0,0 +1,7 @@
+/*
+Suppose you have infinite supply of a given set of coins, each with their own weight and value, but you can only carry a certain amount of weight. Write a function that returns the maximum value of the coins that could be carried with the given total weight.
+*/
+
+function maxCarriedValue() {
+
+}
