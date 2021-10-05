fd44d351ea81a908f821bcc7f77e5286b4b822ed
sossw1 <sossw1@gmail.com>
2021-10-05 16:21:43 -0400

    efficiency

--- dynamic-and-recursion/weighted-value.js
+++ dynamic-and-recursion/weighted-value.js
@@ -2,6 +2,10 @@
 Suppose you have infinite supply of a given set of coins, each with their own weight and value, but you can only carry a certain amount of weight. Write a function that returns the maximum value of the coins that could be carried with the given total weight.
 
   Idea 1: Compose array of best value by weight (index) bottom-up for each coin.
+
+    Efficiency (where n = # coins, w = maximum weight)
+    Time: O(n*w)
+    Space: O(w)
 */
 
 function maxCarriedValue(coinTypes, maxWeight) {
