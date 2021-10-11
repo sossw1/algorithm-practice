fd79a27194e9f6978ba274816d5e76d62a20c0f1
sossw1 <sossw1@gmail.com>
2021-10-11 13:46:57 -0400

    idea 1

--- queue-stack/max-stack.js
+++ queue-stack/max-stack.js
@@ -1,5 +1,7 @@
 /*
 Given the below Stack class, implement a new class MaxStack with a method getMax that returns the largest element in the stack without removing the item. Stacks will only contain integers.
+
+  Idea 1: Use a greedy algorithm by popping elements from the stack into another stack, which will be in reverse order, then pop from that stack back into the original stack so that order is maintained.
 */
 
 class Stack {
