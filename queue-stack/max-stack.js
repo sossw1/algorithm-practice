a96d7688d7b487917b659d49b44b4de9c76cc136
sossw1 <sossw1@gmail.com>
2021-10-11 13:09:15 -0400

    base problem

---
+++ queue-stack/max-stack.js
@@ -0,0 +1,20 @@
+/*
+Given the below Stack class, implement a new class MaxStack with a method getMax that returns the largest element in the stack without removing the item.
+*/
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    if(!this.items.length) return null;
+    return this.items.pop();
+  }
+  peek() {
+    if(!this.items.length) return null;
+    return this.items[this.items.length - 1];
+  }
+}
