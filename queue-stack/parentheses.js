0826a6fb5eee850ba84f9fc64e160b8d839ba09a
sossw1 <sossw1@gmail.com>
2021-10-12 12:16:44 -0400

    idea 1

--- queue-stack/parentheses.js
+++ queue-stack/parentheses.js
@@ -1,5 +1,7 @@
 /*
 Write a function that, given a sentence that may contain nested parentheses and the index of an opening parenthesis, finds the corresponding closing parenthesis.
+
+  Idea 1: Traverse the string to the right of the given opening position, checking for opening or closing parentheses. We want to use LIFO approach to know when nested sets of parentheses are closed. Use a counter to track how many unclosed parentheses have been encountered and return the position when this becomes zero.
 */
 
 function findCloseParenthesis(string, openingPosition) {
