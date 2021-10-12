f607d5973cac28ebf2bbd782e2f6b1ac7f54fa42
sossw1 <sossw1@gmail.com>
2021-10-12 16:46:26 -0400

    efficiency

--- queue-stack/validate-nesting.js
+++ queue-stack/validate-nesting.js
@@ -2,6 +2,10 @@
 Write a function that determines whether or not an input string's opening and closing parentheses, brackets and braces are nested properly.
 
   Idea 1: Iterate through the string using LIFO strategy to check any encountered closing symbol matches the most recent opening symbol. Use a stack to store opening symbols.
+
+    Efficiency
+    Time: O(n)
+    Space: O(n)
 */
 
 class Stack {
