dbdac0b95312e834841a8cd581e84ad2c1fbd1fd
sossw1 <sossw1@gmail.com>
2021-10-12 16:23:39 -0400

    stack class

--- queue-stack/validate-nesting.js
+++ queue-stack/validate-nesting.js
@@ -4,6 +4,19 @@ Write a function that determines whether or not an input string's opening and cl
   Idea 1: Iterate through the string using LIFO strategy to check any encountered closing symbol matches the most recent opening symbol. Use a stack to store opening symbols.
 */
 
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    if (!this.items.length) return null;
+    return this.items.pop();
+  }
+}
+
 function isNestingValid(string) {
 
 }
