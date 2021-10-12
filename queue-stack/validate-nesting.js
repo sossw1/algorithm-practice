3b7ebf22d9432cc7947390c855ed92efd5e6e3fe
sossw1 <sossw1@gmail.com>
2021-10-12 16:22:30 -0400

    base problem

---
+++ queue-stack/validate-nesting.js
@@ -0,0 +1,7 @@
+/*
+Write a function that determines whether or not an input string's opening and closing parentheses, brackets and braces are nested properly.
+*/
+
+function isNestingValid(string) {
+
+}
