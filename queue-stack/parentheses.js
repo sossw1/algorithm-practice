cc1a75bfd86fa1e4814ad1c26767cea6459a3f6a
sossw1 <sossw1@gmail.com>
2021-10-12 11:41:19 -0400

    base problem

---
+++ queue-stack/parentheses.js
@@ -0,0 +1,7 @@
+/*
+Write a function that, given a sentence that may contain nested parentheses and the index of an opening parenthesis, finds the corresponding closing parenthesis.
+*/
+
+function findCloseParenthesis(string, openingPosition) {
+
+}
