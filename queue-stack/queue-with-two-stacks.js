7e5522619520a26b050eb9a8c71dd1483887bffb
sossw1 <sossw1@gmail.com>
2021-10-12 09:22:49 -0400

    idea 2

--- queue-stack/queue-with-two-stacks.js
+++ queue-stack/queue-with-two-stacks.js
@@ -2,6 +2,8 @@
 Implement a queue with two stacks. Your queue should have enqueue and dequeue methods and be FIFO. Assume you already have a stack implementation with push and pop methods operating in O(1) time.
 
   Idea 1: Enqueues push items to one stack while dequeues pop items from the other stack. When switching from an enqueue call to a dequeue call or vice versa, make sure items are transferred to the correct stack.
+
+  Idea 2: Improve upon idea 1. When calling second/third/etc. dequeue/enqueue, simply push/pop from the same stack as the previous operation.
 */
 
 class Stack {
