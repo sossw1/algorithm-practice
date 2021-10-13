ec660d24429cf3c1725126777b20def557519e51
sossw1 <sossw1@gmail.com>
2021-10-13 16:22:09 -0400

    idea 1

--- linked-list/contains-cycle.js
+++ linked-list/contains-cycle.js
@@ -1,5 +1,7 @@
 /*
 Write a function that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
+
+  Idea 1: For each node, check if we've previously encountered it and otherwise add it to the list.
 */
 
 function containsCycle(head) {
