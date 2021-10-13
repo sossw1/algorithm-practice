ffb625ce13c9d74614a3fddd60d7cbbe0bea1c16
sossw1 <sossw1@gmail.com>
2021-10-13 14:36:52 -0400

    class LinkedListNode

--- linked-list/delete-node.js
+++ linked-list/delete-node.js
@@ -4,6 +4,13 @@ Write a function that deletes a node from a singly-linked list, given only a var
   Idea 1: Store the "next" pointer from the node that is to be deleted. Then, traverse through the linked list. Determine at each step if the next node would be the node to be deleted. Once this is true, change the current node's "next" pointer to stored pointer.
 */
 
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 function deleteNode(node) {
 
 }
