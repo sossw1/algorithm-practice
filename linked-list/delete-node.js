a7f64ba03781bb60fcdb1bc2ad60a7fb17c9167b
sossw1 <sossw1@gmail.com>
2021-10-13 15:51:15 -0400

    efficiency

--- linked-list/delete-node.js
+++ linked-list/delete-node.js
@@ -2,6 +2,10 @@
 Write a function that deletes a node from a singly-linked list, given only a variable pointing to that node.
 
   Idea 1: Store the "next" pointer from the node that is to be deleted. Then, traverse through the linked list. Determine at each step if the next node would be the node to be deleted. Once this is true, change the current node's "next" pointer to stored pointer.
+
+    Efficiency
+    Time: O(n)
+    Space: O(1)
 */
 
 class LinkedListNode {
