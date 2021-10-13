85bbcb603a7262ae82d88ab9946b5ca8d39a8ba2
sossw1 <sossw1@gmail.com>
2021-10-13 16:45:49 -0400

    efficiency

--- linked-list/contains-cycle.js
+++ linked-list/contains-cycle.js
@@ -4,6 +4,10 @@ Write a function that takes the first node in a singly-linked list and returns a
   Idea 1: For each node, check if we've previously encountered it and otherwise add it to the list.
 
   Idea 2: Use two pointers, where one "slow" pointer traverses one node at a time, while a "fast" pointer traverses two nodes at a time. If they collide, we have a loop. If the "slow" pointer reaches the end (a null "next"), we terminate knowing there were no loops.
+
+    Efficiency
+    Time: O(n) since in the worst case, the fast pointer traverses the list twice and the slow pointer traverses the list once.
+    Space: O(1)
 */
 
 class LinkedListNode {
