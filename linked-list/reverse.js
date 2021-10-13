4b72551081fb83460bbd88d36f5a311812c1ce87
sossw1 <sossw1@gmail.com>
2021-10-13 18:05:39 -0400

    idea 1

--- linked-list/reverse.js
+++ linked-list/reverse.js
@@ -1,5 +1,7 @@
 /*
 Write a function that takes the existing head of a linked list, reverses the list in place, and returns the new head of the list.
+
+  Idea 1: Walk forward through list, reversing the pointer for each node.
 */
 
 class LinkedListNode {
