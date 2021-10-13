92e1da0c59981bad57613b7ba6c176ce8f071178
sossw1 <sossw1@gmail.com>
2021-10-13 16:53:28 -0400

    base problem

---
+++ linked-list/reverse.js
@@ -0,0 +1,14 @@
+/*
+Write a function that takes the existing head of a linked list, reverses the list in place, and returns the new head of the list.
+*/
+
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function reverseList(head) {
+
+}
