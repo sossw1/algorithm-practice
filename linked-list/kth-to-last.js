8b9af310cfc09fbfc655b7d69144a9fe6957198b
sossw1 <sossw1@gmail.com>
2021-10-14 12:28:24 -0400

    class LinkedListNode

--- linked-list/kth-to-last.js
+++ linked-list/kth-to-last.js
@@ -4,6 +4,13 @@ Write a function that takes an integer k and the head node of a singly-linked li
   Idea 1: Walk through the array with two pointers that are k-1 apart until the second pointer hits the end of the list.
 */
 
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 function kthToLastNode(head, k) {
 
 }
