cad63ee7afa5d390177148c34b6a054435af64b2
sossw1 <sossw1@gmail.com>
2021-10-14 12:31:39 -0400

    efficiency

--- linked-list/kth-to-last.js
+++ linked-list/kth-to-last.js
@@ -2,6 +2,10 @@
 Write a function that takes an integer k and the head node of a singly-linked list and returns the kth to last node in the list.
 
   Idea 1: Walk through the array with two pointers that are k-1 apart until the second pointer hits the end of the list.
+
+    Efficiency
+    Time: O(n)
+    Space: O(1)
 */
 
 class LinkedListNode {
