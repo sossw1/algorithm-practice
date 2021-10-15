1055bf3fe4659a9183b464d5cee3742513d239ea
sossw1 <sossw1@gmail.com>
2021-10-15 16:38:11 -0400

    base problem

---
+++ math/rectangle-intersection.js
@@ -0,0 +1,17 @@
+/*
+Given two rectangles as the coordinates of the bottom left corner and their width and height, write a function that outputs their intersection in the same format.
+*/
+
+
+class Rectangle {
+  constructor(leftX, bottomY, width, height) {
+    leftX,
+    bottomY,
+    width,
+    height
+  }
+}
+
+function intersection(rectangleA, rectangleB) {
+
+}
