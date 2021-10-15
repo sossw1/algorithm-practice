af05f3689317867d0a5e3eb49d4d544d08317ca0
sossw1 <sossw1@gmail.com>
2021-10-15 17:47:46 -0400

    idea 1

--- math/rectangle-intersection.js
+++ math/rectangle-intersection.js
@@ -1,5 +1,7 @@
 /*
 Given two rectangles as the coordinates of the bottom left corner and their width and height, write a function that outputs their intersection in the same format.
+
+  Idea 1: Find range of overlap for both X and Y values. If there is no overlap for either range, then the rectangles do not intersect. If they only intersect at a point, then the intersection of the rectangles is a line or a point. Otherwise, their intersection is a rectangle.
 */
 
 
