3e0b97adb2bbe7512e1662abf4956b4dd793fa91
sossw1 <sossw1@gmail.com>
2021-10-18 19:11:09 -0400

    base problem

---
+++ math/temperature.js
@@ -0,0 +1,14 @@
+/*
+Write a class for a temperature tracker with the following methods:
+1) insert() records a new temperature
+2) getMax() returns the highest temperature recorded
+3) getMin() returns the lowest temperature recorded
+4) getMean() returns the mean of all recorded temperatures
+5) getMode() returns the mode of all recorded temperatures. If there is more than one mode, return all of them.
+
+Be sure to optimize for time and space, favoring speeding up the get methods over the insert method. Temperatures will be reported in Fahrenheit as numbers between 0 and 110.
+*/
+
+class tempTracker {
+
+}
