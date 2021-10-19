c0d30e5de25effa04da5a535ad735dd4d90f0af3
sossw1 <sossw1@gmail.com>
2021-10-19 11:05:10 -0400

    idea 1

--- math/find-duplicate-.js
+++ math/find-duplicate-.js
@@ -1,5 +1,7 @@
 /*
 Given an array of n+1 integers between 1 and n where each number appears once except a duplicate, find the duplicate.
+
+  Idea 1: Using the triangular series from 1 to n, the sum should be [n(n-1)]/2 plus whichever number is the duplicate.
 */
 
 function findDuplicate(arrayOfInts) {
