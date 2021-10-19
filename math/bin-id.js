bdaa8c814047b7d792d8aaba0112b55da3e5bfbd
sossw1 <sossw1@gmail.com>
2021-10-19 10:31:52 -0400

    idea 1

--- math/bin-id.js
+++ math/bin-id.js
@@ -1,5 +1,7 @@
 /*
 Given an array of IDs which contains many duplicate integers and one unique integer, find the unique integer.
+
+  Idea 1: Use a map to store the number of occurrences of integers in the array. Then return the key which has the value of 1.
 */
 
 function findUnique(arrayOfIds) {
