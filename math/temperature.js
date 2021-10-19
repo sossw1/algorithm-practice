3a66afd03e9d84c744b9788864dc3352518b713c
sossw1 <sossw1@gmail.com>
2021-10-18 21:01:26 -0400

    corrected base problem, idea 2

--- math/temperature.js
+++ math/temperature.js
@@ -4,11 +4,13 @@ Write a class for a temperature tracker with the following methods:
 2) getMax() returns the highest temperature recorded
 3) getMin() returns the lowest temperature recorded
 4) getMean() returns the mean of all recorded temperatures
-5) getMode() returns the mode of all recorded temperatures. If there is more than one mode, return all of them.
+5) getMode() returns the mode of all recorded temperatures. If there is more than one mode, return any of them.
 
 Be sure to optimize for time and space, favoring speeding up the get methods over the insert method. Temperatures will be reported in Fahrenheit as numbers between 0 and 110.
 
   Idea 1: Insert temperatures into a map with their occurrences. Upon insertion, update previous values for max, min, mean, modes, number of entries and the number of occurrences of the modes. Retrieval of each get method would then simply retrieve their stored values in the object.
+
+  Idea 2: Simplify tracking temperature occurrences by using an array. Can return any mode, so only update when new entry frequency exceeds mode frequency.
 */
 
 class TempTracker {
